5088228220a021c4424eb3adc006b8636b34cb54
Eri <stayagolubcov@gmail.com>
2021-03-28 21:03:57 +0300

    Add uuid to project Make Card and dynamic render of it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.scss';
+import { useState } from 'react';
 import Header from './components/Header/Header';
+import Card from './components/Card/Card';
+import uuid from 'react-uuid';
+import data from './mock-data.json';
 
 function App() {
+  const [tracked, setTracked] = useState(data);
   return (
     <>
-    <Header/>
-    <main>
-
-    </main>
+      <Header />
+      <main>
+        {!!tracked.length &&
+          tracked.map(elem => <Card key={uuid()} city={elem} />)
+        }
+      </main>
     </>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,24 @@
 import './card.scss';
-
-const Card =()=>{
-
+import Button from '../Button/Button';
+const Card = ({ city }) => {
+    return (
+        <article className="city-card">
+            <header>
+                {city.name}
+            </header>
+            <div>
+                <p>Temperature: {city.temp}&#176;C</p>
+                <p>Humidity: {city.humid}% </p>
+                <p>Pressure: 1015</p>
+                <p>Wind strength and direction: {city.windStr} </p>
+                <p>Last refresh: {city.refresh}</p>
+                <div className="city-card__button-area">
+                    <Button text="Delete" />
+                    <Button text="Refresh" />
+                </div>
+            </div>
+        </article>
+    )
 };
 
 export default Card
