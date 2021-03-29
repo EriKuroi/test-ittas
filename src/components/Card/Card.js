c85b313757553a604342f5ef50991117adf71308
Eri <stayagolubcov@gmail.com>
2021-03-29 16:28:53 +0300

    Change mock data and Card component according to api respond Add weather icons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,17 @@
 import './card.scss';
 import Button from '../Button/Button';
-const Card = ({ city }) => {
+const Card = ({ city, refresh }) => {
     return (
         <article className="city-card">
             <header>
                 {city.name}
             </header>
             <div>
-                <p>Temperature: {city.temp}&#176;C</p>
-                <p>Humidity: {city.humid}% </p>
-                <p>Pressure: 1015</p>
-                <p>Wind strength and direction: {city.windStr} </p>
-                <p>Last refresh: {city.refresh}</p>
+                <p>Temperature: {city.main.temp}&#176;C</p>
+                <p>Humidity: {city.main.humidity}% </p>
+                <p>Pressure: {city.main.pressure}</p>
+                <p>Wind strength and direction: {city.wind.speed} </p>
+                <p>Last refresh: {refresh}</p>
                 <div className="city-card__button-area">
                     <Button text="Delete" />
                     <Button text="Refresh" />
