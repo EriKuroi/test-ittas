390fed53c889cc2d07930d19156f32be7a8ee17e
Eri <stayagolubcov@gmail.com>
2021-03-31 16:23:53 +0300

    Add save to local storage feature fix: findout and add id for Kiev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Card from './components/Card/Card';
 import uuid from 'react-uuid';
 
 function App() {
-  // найті айді кіева
   const [tracked, setTracked] = useState([]);
+  useEffect(() => {
+    const localStorageItem = localStorage.getItem('localStorageString');
+    if (localStorageItem) {
+      const localStorageParsed = JSON.parse(localStorageItem);
+      setTracked(localStorageParsed);
+    }
+  }, [])
   const getChosenCity = async (id) => {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&units=metric&appid=64e98969cdc8ec3dd3bf5d5a19a4b3b5&lang=ru`);
@@ -30,6 +36,8 @@ function App() {
       newTracked.push(data);
     }
     setTracked(newTracked);
+    const localStorageString = JSON.stringify(newTracked);
+    localStorage.setItem('localStorageString', localStorageString);
   }
   const deleteCard = (id) => {
     const index = tracked.findIndex(city => city.id === id)
